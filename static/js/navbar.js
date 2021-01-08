7fe43bd35fcd38a7aea05087861a92b0fba8a5a3
Peter Vuyk <peter.vuyk456@gmail.com>
2021-01-08 18:02:52 -0500

    Fixed navbar and viewing on scroll. Made sectiont title and container fit on screen

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -2,22 +2,25 @@
 window.onscroll = function() {stickyNav()};
 
 // Get the navbar
-var navbar = document.getElementsByClassName("navbar");
+var navbar = d3.select("#nav");
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var sticky = navbar.property("offsetTop");
+var sectionTitle = d3.selectAll("div.section-header");
+var sectionContainer = d3.selectAll("div.section-container");
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickyNav() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-    d3.selectAll("div.section-title").node().classList.add("hide")
+    navbar.classed("sticky", true);
+    sectionTitle.classed("hide", true);
+    sectionContainer.classed("offset", true);
   } else {
-    navbar.classList.remove("sticky");
-    d3.selectAll("div.section-title").node().classList.remove("hide")
+    navbar.classed("sticky", false);
+    sectionTitle.classed("hide", false);
+    sectionContainer.classed("offset", false);
   }
 }
-
 
 // Get navbar options
 var options = d3.selectAll(".nav-link");
@@ -31,9 +34,11 @@ title.on("click", (d, i) => {
   d3.select("#home").style("display", "flex");
 });
 
+var x
 function handleNavClick(d, i) {
   showSection(this);
   setNavBorder(this);
+  d3.event.preventDefault();
 }
 
 function showSection(elem) {
