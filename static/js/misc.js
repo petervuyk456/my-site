489ac8463b6a6cfd2c6cd5c605ed052d438daf3d
Peter Vuyk <peter.vuyk456@gmail.com>
2021-01-12 15:29:15 -0500

    Added qualification, work experience, and resume download link

--- static/js/misc.js
+++ static/js/misc.js
@@ -1,18 +1,18 @@
 // Job experience
 d3.selectAll(".job-header > a").on("click", function(d, i) {
-    window.scrollTo(0, 0);
+    // window.scrollTo(0, 0);
     id = d3.select(this).attr("href");
     d3.selectAll(".collapse").classed("show", false);
     selected = d3.select(id);
     selected.classed("show", selected.classed("show"));
 })
 
-$(document).ready(function() {
-    var hash = window.location.hash;
-    var link = $('a');
-    $('a').click(function(e) {
-      e.preventDefault();
-      hash = link.attr("href");
-      window.location = hash;
-    });
-  });
+// $(document).ready(function() {
+//     var hash = window.location.hash;
+//     var link = $('a');
+//     $('a').click(function(e) {
+//       e.preventDefault();
+//       hash = link.attr("href");
+//       window.location = hash;
+//     });
+//   });
